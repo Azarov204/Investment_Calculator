85fb5506bb96285c3665117c94fec17a03ada5c4
Adam <adamazarov99@gmail.com>
2025-01-19 15:43:30 -0600

    Added a MUI table and styled it a bit. AA

--- src/components/InvestmentTable.tsx
+++ src/components/InvestmentTable.tsx
@@ -1,41 +1,76 @@
 import {SingleMonth} from "../utilities/InvestmentCalculations.tsx";
+import {
+    TableContainer,
+    Table,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell,
+    Paper,
+    styled,
+    tableCellClasses,
+} from "@mui/material";
 
 interface InvestmentProps {
     monthlyInvestmentData: SingleMonth[];
 }
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
 const InvestmentTable = (props: InvestmentProps) => {
 
     return (
-        <div>
-            <h1>Investment Growth Table</h1>
-            {props.monthlyInvestmentData.length > 0 && (
-                <table border={1}>
-                    <thead>
-                    <tr>
-                        <th>Month</th>
-                        <th>Monthly Investment</th>
-                        <th>Total Investment</th>
-                        <th>Monthly Interest</th>
-                        <th>Total Interest</th>
-                        <th>Total Value</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {props.monthlyInvestmentData.map((month) => (
-                        <tr key={month.month}>
-                            <td>{month.month}</td>
-                            <td>{numberWithCommas(month.monthlyInvestment.toFixed(2))}</td>
-                            <td>{numberWithCommas(month.totalInvestment.toFixed(2))}</td>
-                            <td>{numberWithCommas(month.monthlyInterest.toFixed(2))}</td>
-                            <td>{numberWithCommas(month.totalInterest.toFixed(2))}</td>
-                            <td>{numberWithCommas(month.totalValue.toFixed(2))}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
+        <>
+            <h2>Investment Growth Table</h2>
+            {props.monthlyInvestmentData.length > 1 && (
+                <TableContainer component={Paper} sx={{ maxWidth: "700px", maxHeight: "400px" }}>
+                    <Table aria-label="simple table" stickyHeader size={"small"}>
+                        <TableHead>
+                            <TableRow>
+                                <StyledTableCell>Month</StyledTableCell>
+                                <StyledTableCell>Monthly Investment</StyledTableCell>
+                                <StyledTableCell>Total Investment</StyledTableCell>
+                                <StyledTableCell>Monthly Interest</StyledTableCell>
+                                <StyledTableCell>Total Interest</StyledTableCell>
+                                <StyledTableCell>Total Value</StyledTableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                props.monthlyInvestmentData.map((row) => (
+                                    <StyledTableRow key={row.month} sx={{ '&:last-child td, &:last-child th': { border: 0}, height: "10px" }}>
+                                        <StyledTableCell>{row.month}</StyledTableCell>
+                                        <StyledTableCell>${numberWithCommas(row.monthlyInvestment.toFixed(2))}</StyledTableCell>
+                                        <StyledTableCell>${numberWithCommas(row.totalInvestment.toFixed(2))}</StyledTableCell>
+                                        <StyledTableCell>${numberWithCommas(row.monthlyInterest.toFixed(2))}</StyledTableCell>
+                                        <StyledTableCell>${numberWithCommas(row.totalInterest.toFixed(2))}</StyledTableCell>
+                                        <StyledTableCell>${numberWithCommas(row.totalValue.toFixed(2))}</StyledTableCell>
+                                    </StyledTableRow>
+                                ))
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             )}
-        </div>
+        </>
     );
 }
 
