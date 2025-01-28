2c278f4ce637bdd87856d5fc7c60ac1d5b5aa066
Adam <adamazarov99@gmail.com>
2025-01-28 01:15:01 -0600

    Added mui grids for better page layout. AA

--- src/pages/InvestmentCalculator.tsx
+++ src/pages/InvestmentCalculator.tsx
@@ -1,6 +1,6 @@
 import NumberInputField from "../components/NumberInputField.tsx";
 import {useState} from "react";
-import {Button, Paper} from "@mui/material";
+import {Button, Container, Grid2, Paper} from "@mui/material";
 import {calculateMonthlyTable, SingleMonth, MetaData, InvestmentCalculatorResult} from "../utilities/InvestmentCalculations.tsx";
 import InvestmentTable from "../components/InvestmentTable.tsx";
 import {InvestmentGraph} from "../components/InvestmentGraph.tsx";
@@ -33,49 +33,71 @@ export function InvestmentCalculator() {
     }
 
     return (
-        <>
-            <NumberInputField
-                id={"initialInvestment"}
-                label={"Initial Investment"}
-                value={initialInvestment}
-                onChange={setInitialInvestment}
-                tooltipText={"The amount of money you will invest up front"}
-            />
-            <NumberInputField
-                id={"monthlyAddition"}
-                label={"Monthly addition"}
-                value={monthlyAddition}
-                onChange={setMonthlyAddition}
-                tooltipText={"How much money you will add monthly"}
-            />
-            <NumberInputField
-                id={"interestRate"}
-                label={"Interest Rate"}
-                value={interestRate}
-                onChange={setInterestRate}
-                tooltipText={"The interest rate you expect to earn on your investment"}
-            />
-            <NumberInputField
-                id={"yearsToGrow"}
-                label={"Years to grow"}
-                value={yearsToGrow}
-                onChange={setYearsToGrow}
-                tooltipText={"How long you will hold your investment"}
-            />
-            <Button
-                onClick={() => computeAndShowInterestTable()}
-            >
-                Calculate
-            </Button>
+        <Container maxWidth={"xl"}>
+            <Grid2 container spacing={2}>
+                <Grid2 size={12}>
+                    <p>Find out how your investment will grow over time with compound interest</p>
+                </Grid2>
+                <Grid2 size={6}>
+                    <NumberInputField
+                        id={"initialInvestment"}
+                        label={"Initial Investment"}
+                        value={initialInvestment}
+                        onChange={setInitialInvestment}
+                        tooltipText={"The amount of money you will invest up front"}
+                    />
+                </Grid2>
+                <Grid2 size={6}>
+                    <NumberInputField
+                        id={"monthlyAddition"}
+                        label={"Monthly addition"}
+                        value={monthlyAddition}
+                        onChange={setMonthlyAddition}
+                        tooltipText={"How much money you will add monthly"}
+                    />
+                </Grid2>
+                <Grid2 size={6}>
+                    <NumberInputField
+                        id={"interestRate"}
+                        label={"Interest Rate"}
+                        value={interestRate}
+                        onChange={setInterestRate}
+                        tooltipText={"The interest rate you expect to earn on your investment"}
+                    />
+                </Grid2>
+                <Grid2 size={6}>
+                    <NumberInputField
+                        id={"yearsToGrow"}
+                        label={"Years to grow"}
+                        value={yearsToGrow}
+                        onChange={setYearsToGrow}
+                        tooltipText={"How long you will hold your investment"}
+                    />
+                </Grid2>
+                <Grid2 size={12}>
+                    <Button
+                        onClick={() => computeAndShowInterestTable()}
+                        variant={"contained"}
+                    >
+                        Calculate
+                    </Button>
+                </Grid2>
+            </Grid2>
             {showResults && (
-                <>
-                    <ResultDisplay metaData={metaDataResults} />
-                    <InvestmentTable monthlyInvestmentData={monthlyDataResults} />
-                    <Paper sx={{maxWidth: "40%"}}>
-                        <InvestmentGraph monthlyInvestmentData={monthlyDataResults} />
-                    </Paper>
-                </>
+                <Grid2 container>
+                    <Grid2>
+                        <ResultDisplay metaData={metaDataResults} />
+                    </Grid2>
+                    <Grid2>
+                        <InvestmentTable monthlyInvestmentData={monthlyDataResults} />
+                    </Grid2>
+                    <Grid2 size={12}>
+                        <Paper>
+                            <InvestmentGraph monthlyInvestmentData={monthlyDataResults} />
+                        </Paper>
+                    </Grid2>
+                </Grid2>
             )}
-        </>
+        </Container>
     )
 }
